a72ecca7b8d56570a8d2f6d79560503021fc7215
jamesisaiahclementelove <71129320+jamesisaiahclementelove@users.noreply.github.com>
2020-10-14 16:37:05 +0800

    v0.1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// MAIN MENU
+
+function mainFun()
+{
+var x = document.getElementsByClassName("menus");
+var i = document.getElementById("main_btn");
+for(var j=0; j<x.length; j++)
+if( x[j].style.display == "block")
+{
+	x[j].style.display = "none";
+	i.innerHTML = "M<br />E<br />N<br />U";
+	i.style.marginLeft = "";
+}
+else
+{
+	x[j].style.display = "block";
+	i.innerHTML = "H<br />I<br />D<br />E";
+	i.style.marginLeft = "20%";
+}
+}
