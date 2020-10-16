a94851518961bd5310ca614ba481a1ec4b48397b
jamesisaiahclementelove <71129320+jamesisaiahclementelove@users.noreply.github.com>
2020-10-16 23:42:40 +0800

    version 0.6
    dark/light mode.

--- js/script-desktop.js
+++ js/script-desktop.js
@@ -20,8 +20,8 @@ else
 }
 
 
-function subjoc(){
-	
+function subjoc()
+{
 var subjd = document.getElementById('subj');
 if(subjd.style.display === "none")
 {
@@ -31,5 +31,34 @@ else
 {
 	subjd.style.display = "none";
 }
-
 }
+
+function dark_light()
+{
+	var dark = document.getElementsByClassName('dark');
+	for (var light = 0; light < dark.length; light++)
+	
+	if(dark[light].style.background == "")
+	{
+		dark[light].style.background = "#fff";
+		document.getElementById("heading1").style.color="#000";
+	}
+	else
+	{
+		dark[light].style.background = "";
+		document.getElementById("heading1").style.color ="";
+	}
+	
+	var txt_color = document.getElementsByClassName("options");
+	for (var txtt = 0; txtt < txt_color.length; txtt++)
+	
+	if(txt_color[txtt].style.color == "")
+	{
+		txt_color[txtt].style.color="#000";
+	}
+	else
+	{
+		txt_color[txtt].style.color="";
+	}
+}
+
